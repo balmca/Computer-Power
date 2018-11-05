7a45ce0fa08900dec71b065b1990e488b154a4c3
balmca <44289089+balmca@users.noreply.github.com>
2018-11-05 18:25:48 +1300

    Update and rename PAPER_SCISSORS_ROCK.py to Paper, Scissors, Rock.py

--- Paper, Scissors, Rock.py
+++ Paper, Scissors, Rock.py
@@ -2,43 +2,45 @@ import random as random
 user = True
 
 # Welcome and Instructions
-print("Welcome to Paper, Scissors, Rock - you're up against HAL, the Computer!")
+print("\nWelcome to Paper, Scissors, Rock - you're up against HAL, the Computer!")
+
 
 while True:
     # Options and HAL
-    options = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
+    options = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors', 'e': 'Exit'}
     HAL = random.choice(list(options))
+    while HAL == 'e':
+        HAL = random.choice(list(options))
     # User Input and Exit
-    user = raw_input("\nEnter Paper (P), Scissors (S), Rock (R), or Exit (E): ")
-    user = user.lower()
+    user = raw_input("\nEnter Paper (P), Scissors (S), Rock (R), or Exit (E): ").lower()
     if user == 'e':
         print("End Game")
         break
     elif user == 'r':
         print("Your choice: " + options[user])
         print("HAL's choice: " + options[HAL])
-        if HAL == 'r':
-            print("Its a draw!")
-        elif HAL == 'p':
+        if HAL == 'p':
             print("HAL Wins!")
-        else:
+        elif HAL == 's':
             print("You Win!")
+        else:
+            print("Draw!")
     elif user == 'p':
         print("Your choice: " + options[user])
         print("HAL's choice: " + options[HAL])
         if HAL == 'r':
             print("You Win!")
-        elif HAL == 'p':
-            print("Its a draw!")
-        else:
+        elif HAL == 's':
             print("HAL Wins!")
+        else:
+            print("Draw!")
     elif user == 's':
         print("Your choice: " + options[user])
         print("HAL's choice: " + options[HAL])
         if HAL == 'r':
             print("HAL Wins!")
         elif HAL == 'p':
-            print("HAL Wins!")
+            print("You Win!")
         else:
             print("Its a draw!")
     else:
