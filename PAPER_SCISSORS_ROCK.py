0087d422ecb3dfd5119fe6b8b47b4fd650163504
balmca <44289089+balmca@users.noreply.github.com>
2018-11-05 13:01:14 +1300

    Update PAPER_SCISSORS_ROCK.py

--- PAPER_SCISSORS_ROCK.py
+++ PAPER_SCISSORS_ROCK.py
@@ -1,38 +1,40 @@
-import random
+import random as random
 user = True
-
-# Options and HAL
-options = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors', 'e': 'Exit'}
-HAL = random.choice(list(options))
 
 # Welcome and Instructions
 print("Welcome to Paper, Scissors, Rock - you're up against HAL, the Computer!")
 
-# User Input and Exit
 while True:
-    user = input("Enter Paper (P), Scissors (S), Rock (R), or Exit (E): ")
+    # Options and HAL
+    options = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
+    HAL = random.choice(list(options))
+    # User Input and Exit
+    user = raw_input("\nEnter Paper (P), Scissors (S), Rock (R), or Exit (E): ")
     user = user.lower()
     if user == 'e':
         print("End Game")
         break
-    if user == 'r':
-        print("HAL's choice:", HAL)
+    elif user == 'r':
+        print("Your choice: " + options[user])
+        print("HAL's choice: " + options[HAL])
         if HAL == 'r':
             print("Its a draw!")
         elif HAL == 'p':
             print("HAL Wins!")
         else:
             print("You Win!")
-    if user == 'p':
-        print("HAL's choice:", HAL)
+    elif user == 'p':
+        print("Your choice: " + options[user])
+        print("HAL's choice: " + options[HAL])
         if HAL == 'r':
             print("You Win!")
         elif HAL == 'p':
             print("Its a draw!")
         else:
             print("HAL Wins!")
-    if user == 's':
-        print("HAL's choice:", HAL)
+    elif user == 's':
+        print("Your choice: " + options[user])
+        print("HAL's choice: " + options[HAL])
         if HAL == 'r':
             print("HAL Wins!")
         elif HAL == 'p':
@@ -40,5 +42,4 @@ while True:
         else:
             print("Its a draw!")
     else:
-        print("That's made up, this is Paper, Scissors, Rock!")
-
+        print("\nThat's made up, this is Paper, Scissors, Rock!")
